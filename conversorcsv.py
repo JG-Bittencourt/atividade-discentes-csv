ed7c9ca38eae40ea7c8d7d6b7a0561242122bc82
João Gustavo Bittencourt <joaogustavobittencourt01@gmail.com>
2026-04-01 15:10:50 -0300

    Update conversorcsv.py
    Implementação da função salvar txt
    Criação do menu main
    organização do código

--- conversorcsv.py
+++ conversorcsv.py
@@ -1,5 +1,22 @@
 import csv
+from dataclasses import dataclass
 
+#CRIAÇÃO DA ESTRUTURA ALUNO 
+@dataclass
+class Aluno:
+    matricula: int
+    nome:str
+    ano_ingresso: int
+    periodo_ingresso: int
+    tipo_discente: str
+    status_discente: str
+    nivel_ensino: str
+    nome_curso: str
+    modalidade_curso: str
+    nome_unidade: str
+    nome_unidade_gestora: str
+
+#FUNÇÃO PARA IMPRESSÃO DOS DADOS, PARÂMETRO DE BUSCA = MATRÍCULA
 def imprimir (alunos, matricula):
     n = 0
     while n < len(alunos) and alunos[n].matricula != matricula:
@@ -21,49 +38,80 @@ def imprimir (alunos, matricula):
         f"UNIDADE GESTORA: {alunos[n].nome_unidade_gestora}")
 
 
-from dataclasses import dataclass
+#FUNÇÃO SALVAR COMO TEXTO
 
-@dataclass
-class Aluno:
-    matricula: int
-    nome:str
-    ano_ingresso: int
-    periodo_ingresso: int
-    tipo_discente: str
-    status_discente: str
-    nivel_ensino: str
-    nome_curso: str
-    modalidade_curso: str
-    nome_unidade: str
-    nome_unidade_gestora: str
+def savetxt (alunos):
+
+    try:
+        with open ("arqconvert.txt","w", encoding="UTF-8") as arquivotxt:
+            for aluno in alunos:
+                print(f"MATRÍCULA: {aluno.matricula}\n"
+                f"NOME: {aluno.nome}\n"
+                f"ANO DE INGRESSO: {aluno.ano_ingresso}\n"
+                f"PERÍODO DE INGRESSO: {aluno.periodo_ingresso}\n"
+                f"TIPO DE DISCENTE: {aluno.tipo_discente}\n"
+                f"STATUS: {aluno.status_discente}\n"
+                f"NÍVEL DE ENSINO: {aluno.nivel_ensino}\n"
+                f"CURSO: {aluno.nome_curso}\n"
+                f"MODALIDADE: {aluno.modalidade_curso}\n"
+                f"UNIDADE: {aluno.nome_unidade}\n"
+                f"UNIDADE GESTORA: {aluno.nome_unidade_gestora}\n", file = arquivotxt)
+
+        if arquivotxt != None:
+            print ("Arquivo txt salvo com sucesso.")
+
+    except:
+        print("Erro ao salvar o arquivo.")
 
 alunos = []
 
-with open("dis-csv-discentes-de-graduacao-de-2025_1.csv", encoding="UTF-8") as arquivocsv:
-
-    if arquivocsv == None:
-        print("=============================\nFalha ao carregar o arquivo\n=============================")
-    else:
+try:
+    with open("C:/Users/POSITIVO/projetos/dis-csv-discentes-de-graduacao-de-2025_1.csv", encoding="UTF-8") as arquivocsv:
         print("=============================\nArquivo carregado com sucesso\n=============================") 
+        leitor = csv.DictReader(arquivocsv,delimiter=",")
 
-    leitor = csv.DictReader(arquivocsv,delimiter=",")
+        for linha in leitor:
+            aluno = Aluno(
+                matricula=int(linha["matricula"]),
+                nome=linha["nome_discente"],
+                ano_ingresso=int(linha["ano_ingresso"]),
+                periodo_ingresso=int(linha["periodo_ingresso"]),
+                tipo_discente=linha["tipo_discente"],
+                status_discente=linha["status_discente"],
+                nivel_ensino=linha["nivel_ensino"],
+                nome_curso=linha["nome_curso"],
+                modalidade_curso=linha["modalidade_educacao"],
+                nome_unidade=linha["nome_unidade"],
+                nome_unidade_gestora=linha["nome_unidade_gestora"]
+            )
+            alunos.append(aluno)
 
-    for linha in leitor:
-        aluno = Aluno(
-            matricula=int(linha["matricula"]),
-            nome=linha["nome_discente"],
-            ano_ingresso=int(linha["ano_ingresso"]),
-            periodo_ingresso=int(linha["periodo_ingresso"]),
-            tipo_discente=linha["tipo_discente"],
-            status_discente=linha["status_discente"],
-            nivel_ensino=linha["nivel_ensino"],
-            nome_curso=linha["nome_curso"],
-            modalidade_curso=linha["modalidade_educacao"],
-            nome_unidade=linha["nome_unidade"],
-            nome_unidade_gestora=linha["nome_unidade_gestora"]
-        )
-        alunos.append(aluno)
+except:
+    print("=============================\nFalha ao carregar o arquivo\n=============================")
 
-matriculadiscente = int(input("Informe a matricula do discente para obtenção dos dados: "))
-print("=========================================================================")
-imprimir(alunos,matriculadiscente)
+
+
+def main():
+
+    while True:
+        option = int(input("============================================================\n"
+                        "Informe o n° da opção desejada:\n"
+                        "1 - Exibir dados de Aluno através da matrícula.\n"
+                        "2 - Salvar arquivo como txt.\n"
+                        "3 - Sair\n"
+                        "============================================================\n"))
+        
+        if option == 1:
+            matriculadiscente = int(input("Informe a matricula do discente para obtenção dos dados: "))
+            print("=========================================================================")
+            imprimir(alunos,matriculadiscente) 
+        
+        elif option == 2:
+            savetxt(alunos)
+        elif option == 3:
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida.")
+
+main()
